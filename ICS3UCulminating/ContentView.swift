457acc736c0e5ac0f06f01eb2aab1fff557bdead
Stella Shin <sshin@lcs.on.ca>
2026-06-01 11:38:31 -0400

    Initial Commit

---
+++ ICS3UCulminating/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ICS3UCulminating
+//
+//  Created by zimmer_h on 6/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
